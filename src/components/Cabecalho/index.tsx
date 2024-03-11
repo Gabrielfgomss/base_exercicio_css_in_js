7014ce01870e8187272f6053910a48352a571b95
Gabriel Gomes <gabriel.gomes_fernandes@hotmail.com>
2024-03-10 21:29:57 -0300

    feature: adding styled components based styles

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,17 @@
+import styled from 'styled-components'
 import styles from './Cabecalho.module.css'
 
+const CabecalhoContainer = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <CabecalhoContainer>
     <h1>EBAC Jobs</h1>
-  </header>
+  </CabecalhoContainer>
 )
 
 export default Cabecalho
